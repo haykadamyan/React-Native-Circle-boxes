e52e8ee4236ab3cc4311cffe75c7d3d8be94cb54
Varik <varikmatevosyan@gmail.com>
2017-04-12 18:45:31 +0400

    bubbles alpha ver

--- src/index.js
+++ src/index.js
@@ -15,13 +15,19 @@ import {
 
 import Styles from './styles';
 
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max-min+1) + min)
+}
+
 function generateBubble({radius, style}) {
     return (
-        <TouchableOpacity style={[{width: radius, height: radius}, Styles.bubble, style]}>
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Text</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={{paddingTop: randomBetween(5, 100), paddingLeft: randomBetween(5, 100)}}>
+            <TouchableOpacity style={[{width: radius, height: radius}, Styles.bubble, style]}>
+                <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                    <Text>Text</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
     )
 }
 
@@ -32,12 +38,12 @@ export default class ReactNativeCircleBoxes extends Component {
                 <ScrollView horizontal={true} contentContainerStyle={{flex: 1, flexDirection: 'column', flexWrap:'wrap',  backgroundColor: 'black', alignItems:'flex-start'}}>
                     {generateBubble({radius: 150, style: {backgroundColor: 'purple'} })}
                     {generateBubble({radius: 150, style: {backgroundColor: 'blue'} })}
-                    {generateBubble({radius: 150, style: {backgroundColor: 'pink'} })}
+                    {generateBubble({radius: 250, style: {backgroundColor: 'pink'} })}
                     {generateBubble({radius: 150, style: {backgroundColor: 'red'} })}
-                    {generateBubble({radius: 150, style: {backgroundColor: 'red'} })}
-                    {generateBubble({radius: 150, style: {backgroundColor: 'red'} })}
-                    {generateBubble({radius: 150, style: {backgroundColor: 'red'} })}
-                    {generateBubble({radius: 150, style: {backgroundColor: 'red'} })}
+                    {generateBubble({radius: 50, style: {backgroundColor: 'red'} })}
+                    {generateBubble({radius: 80, style: {backgroundColor: 'red'} })}
+                    {generateBubble({radius: 350, style: {backgroundColor: 'red'} })}
+                    {generateBubble({radius: 75, style: {backgroundColor: 'red'} })}
                 </ScrollView>
             </View>
         );
